06063cc7ee0c78b4384b0e8c6fbdb0ba599d66e3
Sterling Jenkins <stelring.silverray@outlook.com>
2022-10-27 16:00:21 -0600

    Added Finished Scroll View 'ISpy' lab

--- 1 - Tables and Persistence/6 - Intermediate Table Views/lab/FavoriteBooks/FavoriteBooks/BookTableViewCell.swift
+++ 1 - Tables and Persistence/6 - Intermediate Table Views/lab/FavoriteBooks/FavoriteBooks/BookTableViewCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class BookTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func update(with book: Book) {
+        titleLabel.text = book.title
+        descriptionLabel.text = book.description
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- 1 - Tables and Persistence/6 - Intermediate Table Views/lab/FavoriteBooks/FavoriteBooks/BookTableViewController.swift
+++ 1 - Tables and Persistence/6 - Intermediate Table Views/lab/FavoriteBooks/FavoriteBooks/BookTableViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class BookTableViewController: UITableViewController {
     
-    var books: [Book] = []
+    var books: [Book] = [Book(title: "Harry Potter and the Sorcerer's Stone", author: "J. K. Rowling", genre: "Fantasy", length: "320")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,41 +22,63 @@ class BookTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookTableViewCell
 
         let book = books[indexPath.row]
-        var content = cell.defaultContentConfiguration()
-        content.text = book.title
-        content.secondaryText = book.description
-        cell.contentConfiguration = content
+//        var content = cell.defaultContentConfiguration()
+//        content.text = book.title
+//        content.secondaryText = book.description
+//        cell.contentConfiguration = content
+        
+        cell.update(with: book)
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            books.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
 
     // MARK: - Navigation
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
-        guard let source = segue.source as? BookFormViewController,
+        guard let source = segue.source as? BookFormTableViewController,
             let book = source.book else {return}
         
-        if let indexPath = tableView.indexPathForSelectedRow {
-            books.remove(at: indexPath.row)
-            books.insert(book, at: indexPath.row)
-            tableView.deselectRow(at: indexPath, animated: true)
+//        if let indexPath = tableView.indexPathForSelectedRow {
+
+//        } else {
+//
+//        }
+        
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+//            books.remove(at: selectedIndexPath.row)
+//            books.insert(book, at: selectedIndexPath.row)
+//            tableView.deselectRow(at: selectedIndexPath, animated: true)
+            /// ^^^ Origional ||| vvv Emoji
+            books[selectedIndexPath.row] = book
+            tableView.reloadRows(at: [selectedIndexPath], with: .none)
+
         } else {
             books.append(book)
+            /// ^^^ Origional ||| vvv Emoji
+//            let newIndexPath = IndexPath(row: books.count, section: 0)
+//            books.append(book)
+//            tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
     }
     
-    @IBSegueAction func editBook(_ coder: NSCoder, sender: Any?) -> BookFormViewController? {
-        
+    @IBSegueAction func editBook(_ coder: NSCoder, sender: Any?) -> BookFormTableViewController? {
         guard let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell) else {
             return nil
         }
         
         let book = books[indexPath.row]
         
-        return BookFormViewController(coder: coder, book: book)
+        return BookFormTableViewController(coder: coder, book: book)
     }
     
     
